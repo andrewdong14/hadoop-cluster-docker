17af695691bf9bb23159d63def8f536142a7672c
Zhongli Dong <zhongli.dong@gmail.com>
2016-10-19 02:34:33 +1100

    Remove "delete scripts"

--- config/runWC.sh
+++ config/runWC.sh
@@ -2,12 +2,7 @@
 
 # test the hadoop cluster by running wordcount
 
-# delete 
-
---ignore-fail-on-non-empty
-
 # create input files 
-rm -rf input
 mkdir input
 echo "Macquarie MRes COMP777 S22016" >input/file1.txt
 echo "Macquarie MRes S22016" >input/file2.txt
@@ -16,8 +11,6 @@ echo "Macquarie Young Choon Lee" >input/file4.txt
 echo "S22016 Young Choon" >input/file5.txt
 
 
-# delete input directory on HDFS if exists
-hadoop fs -rmdir --ignore-fail-on-non-empty input
 # create input directory on HDFS
 hadoop fs -mkdir -p input
 
